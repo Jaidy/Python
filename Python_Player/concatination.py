a3c17527f4b802d29baa07d2850bb187c73f12eb
Jaidy <32952754+Jaidy@users.noreply.github.com>
2017-10-20 16:23:11 +0530

    Add files via upload

---
+++ Python_Player/concatination.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Jaidheep
+#
+# Created:     20/10/2017
+# Copyright:   (c) 14cse008 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# simple concatination program
+def main():
+    a="Hi everyone"
+    print a + " I'm Jaidheep"
+
+if __name__ == '__main__':
+    main()
