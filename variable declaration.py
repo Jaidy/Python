75f44c57f43ca2ea3348738d8d16cb466b999100
Jaidy <32952754+Jaidy@users.noreply.github.com>
2017-10-20 16:15:42 +0530

    Add files via upload

---
+++ variable declaration.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      14cse008
+#
+# Created:     20/10/2017
+# Copyright:   (c) 14cse008 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# A simple program about declaring variables
+a=2
+b=4
+c=6
+d=8
+e=10
+f=12
+g=14
+h=16
+i=18
+j=20
+add = a+b
+subtract = i-f
+mul = c*d
+div = j/a
+exp = e**a
+print("The sum of a and b is:"+ str(add))
+print("The diffrence between i and f is:" + str(subtract))
+print("The multiplication of c and d is:" + str(mul))
+print("The division of j and a is:" + str(div))
+print("The exponent of e and a is:"+str(exp))
